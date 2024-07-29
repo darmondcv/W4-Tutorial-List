6730c826c0b56f30e67455f9876be9c25bac4cee
Dang <darmondcv@gmail.com>
2024-07-29 14:31:06 +0700

    Initial Commit

---
+++ W4-Tutorial-List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  W4-Tutorial-List
+//
+//  Created by Darmond on 29/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ W4-Tutorial-List/W4_Tutorial_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  W4_Tutorial_ListApp.swift
+//  W4-Tutorial-List
+//
+//  Created by Darmond on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct W4_Tutorial_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
